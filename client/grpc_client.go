11b62c447e62305a9022b10df6f8673acb9dde5c
x9nu <light_blue_dre@163.com>
2024-07-13 21:02:21 +0800

    双向认证

--- client/grpc_client.go
+++ client/grpc_client.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"grpc-test/client/service"
 	"log"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	// 添加证书
-	creds, errCreds := credentials.NewClientTLSFromFile("../cert/server.crt", "*.x9nu.cn")
-	if errCreds != nil {
-		log.Fatal("client:creds err", errCreds)
-	}
+	// // 添加证书 - 单向认证
+	// creds, errCreds := credentials.NewClientTLSFromFile("../cert/server.crt", "*.x9nu.cn")
+	// if errCreds != nil {
+	// 	log.Fatal("client:creds err", errCreds)
+	// }
+
+	cert, _ := tls.LoadX509KeyPair("../cert/client.pem", "../cert/client.key")
+	// 创建一个新的、空的 CertPool
+	certPool := x509.NewCertPool()
+	ca, _ := os.ReadFile("../cert/ca.crt")
+	// 尝试解析所传入的 PEM 编码的证书。如果解析成功会将其加到 CertPool 中，便于后面的使用
+	certPool.AppendCertsFromPEM(ca)
+	// 构建基于 TLS 的 TransportCredentials 选项
+	creds := credentials.NewTLS(&tls.Config{
+		// 设置证书链，允许包含一个或多个
+		Certificates: []tls.Certificate{cert},
+		// 要求必须校验客户端的证书。可以根据实际情况选用以下参数
+		ServerName: "*.x9nu.cn",
+		RootCAs:    certPool,
+	})
+
 	conn, err := grpc.NewClient(":8002", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal("conn err")
--- grpc-server.go
+++ grpc-server.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"grpc-test/service"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	// 添加证书
-	creds, errCreds := credentials.NewServerTLSFromFile("./cert/server.crt", "./cert/server.key")
-	if errCreds != nil {
-		log.Fatal("server:证书生成失败", errCreds)
+	// // 添加证书 - 单向认证
+	// creds, errCreds := credentials.NewServerTLSFromFile("./cert/server.crt", "./cert/server.key")
+	// if errCreds != nil {
+	// 	log.Fatal("server:证书生成失败", errCreds)
+	// }
+
+	cert, err := tls.LoadX509KeyPair("cert/server.crt", "cert/server.key")
+	if err != nil {
+		log.Fatal("证书读取错误", err)
 	}
+	// 创建一个新的、空的 CertPool
+	certPool := x509.NewCertPool()
+	ca, err := os.ReadFile("cert/ca.crt")
+	if err != nil {
+		log.Fatal("ca证书读取错误", err)
+	}
+	// 尝试解析所传入的 PEM 编码的证书。如果解析成功会将其加到 CertPool 中，便于后面的使用
+	certPool.AppendCertsFromPEM(ca)
+	// 构建基于 TLS 的 TransportCredentials 选项
+	creds := credentials.NewTLS(&tls.Config{
+		// 设置证书链，允许包含一个或多个
+		Certificates: []tls.Certificate{cert},
+		// 要求必须校验客户端的证书。可以根据实际情况选用以下参数
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		// 设置根证书的集合，校验方式使用 ClientAuth 中设定的模式
+		ClientCAs: certPool,
+	})
 
 	rpcServer := grpc.NewServer(grpc.Creds(creds))
 	service.RegisterProductServiceServer(rpcServer, service.ProductService)
